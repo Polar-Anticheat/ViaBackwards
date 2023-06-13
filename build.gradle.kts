89f6f9f26ab4cae6796c6eae01b00c6c4bf22314
Souvlakis <soouvlaki@yandex.ru>
2023-06-13 18:13:58 +0300

    Update to 4.7.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 allprojects {
     group = "com.viaversion"
-    version = "4.7.1-SNAPSHOT"
+    version = "4.7.1-SNAPSHOT+polar-fix"
     description = "Allow older clients to join newer server versions."
 }
 
